a46145f6c0d8ec7bf2ab180a5dd5df604ad3667b
Anamika Singh <asingh@birst.com>
2021-10-30 20:08:38 +0530

    Added TestNG for reporting and using maven as build tool

---
+++ src/main/PacktMaiMenuPageElements.java
@@ -0,0 +1,9 @@
+package main;
+
+public class PacktMaiMenuPageElements {
+	
+	public static void name() {
+		
+	}
+
+}
---
+++ test/test/PacktMaiMenuPageTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class PacktMaiMenuPageTest {
+	
+	@Test
+	public void sample()
+	{
+	
+	System.setProperty("webdriver.chrome.driver","/Users/asingh9/Downloads/chromedriver");
+	
+	WebDriver driver = new ChromeDriver();
+	}
+
+}
